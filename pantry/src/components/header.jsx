fab14e666785d3476b4257c760fefc18ce1c0b5e
Shruthi  Tuplur <shruchainz3@Shruthis-MacBook-Air.local>
2023-08-26 15:42:20 -0700

    header initialized with logo

---
+++ pantry/src/components/header.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+
+const Header = () => {
+
+return(
+    <div id='header-main-div'>
+        <img id='pantry-main-logo' src={require("./pantry-main-logo.png")}/>
+    </div>
+)
+
+
+
+
+
+}
+
+export default Header;
