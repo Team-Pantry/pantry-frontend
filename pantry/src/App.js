7f7ba8bb34b6ef614d49aeea428c90693e2aa828
Shruthi  Tuplur <shruchainz3@Shruthis-MacBook-Air.local>
2023-08-26 16:59:44 -0700

    created landing page with image, blurb, and button that will link to login page

--- pantry/src/App.js
+++ pantry/src/App.js
@@ -1,14 +1,24 @@
 
 import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import {
-  Header
+  Header,
+  LandingPage
 } from './components'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
+        <Routes>
+        <Route exact path='/' element={
+          <LandingPage />
+        }>
+        </Route>
+        </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ pantry/src/components/landingpage.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+const LandingPage = () => {
+
+return(
+    <div id="landing-main">
+        <div id='landing-blurb-div'>
+            <p id="landing-blurb">It takes a village.</p>
+            <div id='landing-button-div'>
+                <button className="button" id="landing-button">Start sharing</button>
+            </div>
+        </div>
+        <img id='landing-img' src="https://hips.hearstapps.com/hmg-prod/images/healthy-fresh-rainbow-colored-fruits-and-vegetables-royalty-free-image-1651630109.jpg?crop=0.668xw:1.00xh;0.0799xw,0&resize=1200:*" />
+    </div>
+)    
+}
+
+export default LandingPage;
