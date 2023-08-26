5413984eec65f269be69f5bca3102915ac505b9b
Shruthi  Tuplur <shruchainz3@Shruthis-MacBook-Air.local>
2023-08-26 15:57:09 -0700

    added in nav bar with inactive nav links and some nav bar styling

--- pantry/src/components/header.jsx
+++ pantry/src/components/header.jsx
@@ -6,6 +6,12 @@ const Header = () => {
 return(
     <div id='header-main-div'>
         <img id='pantry-main-logo' src={require("./pantry-main-logo.png")}/>
+        <div id="nav-bar">
+            <p className="nav-bar-links"> Dashboard</p>
+            <p className="nav-bar-links"> Trade Requests</p>
+            <p className="nav-bar-links"> Search</p>
+            <p className="nav-bar-links"> Sign Up/Login</p>
+        </div>
     </div>
 )
 
