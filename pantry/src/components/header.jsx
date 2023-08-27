d8460d5519750be1cac8fc35f8cd4c43a887318a
Shruthi  Tuplur <shruchainz3@Shruthis-MacBook-Air.local>
2023-08-27 12:42:09 -0700

    fixed spacing on landing page, deleted search link from header

--- pantry/src/components/header.jsx
+++ pantry/src/components/header.jsx
@@ -10,7 +10,6 @@ return(
         <div id="nav-bar">
             <Link to='/dashboard'><p className="nav-bar-links"> Dashboard</p></Link>
             <Link to='/swap'><p className="nav-bar-links"> Food swap</p></Link>
-            <p className="nav-bar-links"> Search</p>
             <Link to='/users/register'><p className="nav-bar-links"> Logout</p></Link>
         </div>
     </div>
