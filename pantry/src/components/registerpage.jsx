b8f6a1239eb93b7a282f9753cf8a489d404a1e8c
Shruthi  Tuplur <shruchainz3@Shruthis-MacBook-Air.local>
2023-08-26 17:59:05 -0700

    created nonfunctional register page

--- pantry/src/components/registerpage.jsx
+++ pantry/src/components/registerpage.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Register = () => {
     return(
         <div id='register-main-div'>
-            <p>Register</p>
+            <div id='register-content-container'>
+                <p id='register-title'>Join the Pantry family today</p>
+                <form id='register-form'>
+                    <label htmlFor="username" className="user-input-label">Username</label>
+                    <input type='text' name="username" className="user-input"></input>
+                    <label htmlFor="password" className="user-input-label">Password</label>
+                    <input type='text' name="password" className="user-input"></input>
+                    <label htmlFor="confirm-password" className="user-input-label">Confirm password</label>
+                    <input type='text' name="confirm-password" className="user-input"></input>
+                    <div id='register-button-div'>
+                    <button className="button" type="submit">Create account</button>
+                    </div>
+                </form>
+                <p id='already-reg'>Already have an account? Sign in instead.</p>
+            </div>
         </div>
     )
 }
