45a4b9e6e8d5e5040b591bb13b4ecda63a51b643
Shruthi  Tuplur <shruchainz3@Shruthis-MacBook-Air.local>
2023-08-26 19:53:13 -0700

    added title for dashboard page, added item name and shared by sections too

--- pantry/src/components/dashboard.jsx
+++ pantry/src/components/dashboard.jsx
@@ -24,13 +24,15 @@ const Dashboard = () => {
     }, [])
     return(
         <div id='dashboard-main-div'>
+            <h1 id='dashboard-title'>What's In Our Pantry Today</h1>
             <div id='main-shelf'>
                 {
                     items.map(item => {
                         return(
                             <div key={item.id} className="item">
                                 <img className='item-image' src={item.url} />
-
+                                <p className="item-name">Item Title</p>
+                                <p className="shared-by">Shared by Shruthi</p>
                             </div>
                         )
                     })
